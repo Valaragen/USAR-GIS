43ecf44e1b65d8bf7626384bdd28cc622a6e3575
Valaragen <rudy.boinnard@gmail.com>
2020-07-16 00:40:24 +0200

    setup gateway token augmentation, role converter, webflux security

--- gateway-server/src/main/java/com/usargis/gatewayserver/config/SecurityConfig.java
+++ gateway-server/src/main/java/com/usargis/gatewayserver/config/SecurityConfig.java
@@ -1,23 +1,39 @@
 package com.usargis.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
 
-@Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        // Authenticate through configured OpenID Provider
-        http.oauth2ResourceServer().jwt();
-        // Require authentication for all requests
-        http.authorizeExchange().anyExchange().authenticated();
-        // Allow showing /home within a frame
-        http.headers().frameOptions().mode(XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN);
-        // Disable CSRF in the gateway to prevent conflicts with proxied service CSRF
-        http.csrf().disable();
+        http
+                .httpBasic().disable()
+                .formLogin().disable()
+                .csrf().disable()
+                .logout().disable()
+                .authorizeExchange()
+                .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                .pathMatchers("/usargis-api/**").hasRole("MEMBER")
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(getJwtAuthenticationConverter());
         return http.build();
     }
+
+    private ReactiveJwtAuthenticationConverter getJwtAuthenticationConverter() {
+        ReactiveJwtAuthenticationConverter reactiveJwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        reactiveJwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new ReactiveJwtGrantedAuthoritiesConverterAdapter(new KeycloakRealmRoleConverter()));
+        return reactiveJwtAuthenticationConverter;
+    }
+
 }
+
