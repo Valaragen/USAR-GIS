6931dfe70487f694b58ba6f1a7eb2f0edfbe5b31
Valaragen <rudy.boinnard@gmail.com>
2020-07-21 22:58:12 +0200

    update swagger documentation

--- usargis-api/src/main/java/com/usargis/usargisapi/UsargisApiApplication.java
+++ usargis-api/src/main/java/com/usargis/usargisapi/UsargisApiApplication.java
@@ -9,7 +9,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableEncryptableProperties
-@EnableSwagger2
 public class UsargisApiApplication {
 
     public static void main(String[] args) {
