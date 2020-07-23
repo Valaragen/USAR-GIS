36117b7dd6f60276020731aede46e328c5f54ab8
Valaragen <rudy.boinnard@gmail.com>
2020-07-23 12:52:00 +0200

    create and update method implementation on TeamMemberService

--- usargis-api/src/main/java/com/usargis/usargisapi/service/impl/TeamMemberServiceImpl.java
+++ usargis-api/src/main/java/com/usargis/usargisapi/service/impl/TeamMemberServiceImpl.java
@@ -3,10 +3,16 @@ package com.usargis.usargisapi.service.impl;
 import com.usargis.usargisapi.core.dto.TeamMemberDto;
 import com.usargis.usargisapi.core.model.TeamMember;
 import com.usargis.usargisapi.repository.TeamMemberRepository;
+import com.usargis.usargisapi.service.contract.ModelMapperService;
 import com.usargis.usargisapi.service.contract.TeamMemberService;
+import com.usargis.usargisapi.service.contract.TeamService;
+import com.usargis.usargisapi.service.contract.UserInfoService;
+import com.usargis.usargisapi.util.ErrorConstant;
+import com.usargis.usargisapi.web.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.MessageFormat;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,10 +20,17 @@ import java.util.Optional;
 public class TeamMemberServiceImpl implements TeamMemberService {
 
     private TeamMemberRepository teamMemberRepository;
+    private UserInfoService userInfoService;
+    private TeamService teamService;
+    private ModelMapperService modelMapperService;
 
     @Autowired
-    public TeamMemberServiceImpl(TeamMemberRepository teamMemberRepository) {
+    public TeamMemberServiceImpl(TeamMemberRepository teamMemberRepository, UserInfoService userInfoService,
+                                 TeamService teamService, ModelMapperService modelMapperService) {
         this.teamMemberRepository = teamMemberRepository;
+        this.userInfoService = userInfoService;
+        this.teamService = teamService;
+        this.modelMapperService = modelMapperService;
     }
 
     @Override
@@ -42,11 +55,30 @@ public class TeamMemberServiceImpl implements TeamMemberService {
 
     @Override
     public TeamMember create(TeamMemberDto.PostRequest createDto) {
-        return null;
+        TeamMember teamMemberToCreate = new TeamMember();
+        teamMemberToCreate.setUserInfo(
+                userInfoService.findByUsername(createDto.getUserInfoUsername())
+                        .orElseThrow(() -> new NotFoundException(
+                                MessageFormat.format(ErrorConstant.NO_USER_FOUND_FOR_USERNAME, createDto.getUserInfoUsername())
+                        ))
+        );
+        teamMemberToCreate.setTeam(
+                teamService.findById(createDto.getTeamId())
+                        .orElseThrow(() -> new NotFoundException(
+                                MessageFormat.format(ErrorConstant.NO_TEAM_FOUND_FOR_ID, createDto.getTeamId())
+                        ))
+        );
+        modelMapperService.map(createDto, teamMemberToCreate);
+        return save(teamMemberToCreate);
     }
 
     @Override
     public TeamMember update(Long id, TeamMemberDto.PostRequest updateDto) {
-        return null;
+        TeamMember teamMemberToUpdate = findById(id).orElseThrow(() -> new NotFoundException(
+                        MessageFormat.format(ErrorConstant.NO_TEAM_MEMBER_FOUND_FOR_ID, id)
+                )
+        );
+        modelMapperService.map(updateDto, teamMemberToUpdate);
+        return save(teamMemberToUpdate);
     }
 }
--- usargis-api/src/test/java/com/usargis/usargisapi/service/impl/TeamMemberServiceImplTest.java
+++ usargis-api/src/test/java/com/usargis/usargisapi/service/impl/TeamMemberServiceImplTest.java
@@ -1,13 +1,28 @@
 package com.usargis.usargisapi.service.impl;
 
+import com.usargis.usargisapi.core.dto.TeamMemberDto;
+import com.usargis.usargisapi.core.model.Team;
 import com.usargis.usargisapi.core.model.TeamMember;
+import com.usargis.usargisapi.core.model.UserInfo;
 import com.usargis.usargisapi.repository.TeamMemberRepository;
+import com.usargis.usargisapi.service.contract.ModelMapperService;
 import com.usargis.usargisapi.service.contract.TeamMemberService;
+import com.usargis.usargisapi.service.contract.TeamService;
+import com.usargis.usargisapi.service.contract.UserInfoService;
+import com.usargis.usargisapi.util.ErrorConstant;
+import com.usargis.usargisapi.util.objectMother.dto.TeamMemberDtoMother;
+import com.usargis.usargisapi.util.objectMother.model.TeamMemberMother;
+import com.usargis.usargisapi.util.objectMother.model.TeamMother;
+import com.usargis.usargisapi.util.objectMother.model.UserInfoMother;
+import com.usargis.usargisapi.web.exception.NotFoundException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.AdditionalAnswers;
 import org.mockito.Mockito;
 
+import java.text.MessageFormat;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -17,10 +32,13 @@ class TeamMemberServiceImplTest {
     private TeamMemberService objectToTest;
 
     private TeamMemberRepository teamMemberRepository = Mockito.mock(TeamMemberRepository.class);
+    private UserInfoService userInfoService = Mockito.mock(UserInfoService.class);
+    private TeamService teamService = Mockito.mock(TeamService.class);
+    private ModelMapperService modelMapperService = Mockito.mock(ModelMapperService.class);
 
     @BeforeEach
     void setup() {
-        objectToTest = new TeamMemberServiceImpl(teamMemberRepository);
+        objectToTest = new TeamMemberServiceImpl(teamMemberRepository, userInfoService, teamService, modelMapperService);
     }
 
 
@@ -70,4 +88,114 @@ class TeamMemberServiceImplTest {
         Mockito.verify(teamMemberRepository).delete(teamMemberToDelete);
     }
 
+    @Nested
+    class createTest {
+        private TeamMemberDto.PostRequest teamMemberPostRequestDto = TeamMemberDtoMother.postRequestSample().build();
+        private UserInfo userInfoToLink = UserInfoMother.sampleAuthor().build();
+        private Team teamToLink = TeamMother.sample().build();
+        private TeamMember savedTeamMember = TeamMemberMother.sample().build();
+
+        @BeforeEach
+        void setup() {
+            Mockito.when(userInfoService.findByUsername(teamMemberPostRequestDto.getUserInfoUsername())).thenReturn(Optional.of(userInfoToLink));
+            Mockito.when(teamService.findById(teamMemberPostRequestDto.getTeamId())).thenReturn(Optional.of(teamToLink));
+            Mockito.when(teamMemberRepository.save(Mockito.any(TeamMember.class))).thenReturn(savedTeamMember);
+        }
+
+        @Test
+        void create_noUserForGivenUsername_throwNotFoundException() {
+            Mockito.when(userInfoService.findByUsername(teamMemberPostRequestDto.getUserInfoUsername())).thenReturn(Optional.empty());
+
+            Assertions.assertThatThrownBy(() -> {
+                objectToTest.create(teamMemberPostRequestDto);
+            }).isInstanceOf(NotFoundException.class)
+                    .hasMessage(MessageFormat.format(ErrorConstant.NO_USER_FOUND_FOR_USERNAME, teamMemberPostRequestDto.getUserInfoUsername()));
+        }
+
+        @Test
+        void create_noTeamForGivenTeamId_throwNotFoundException() {
+            Mockito.when(teamService.findById(teamMemberPostRequestDto.getTeamId())).thenReturn(Optional.empty());
+
+            Assertions.assertThatThrownBy(() -> {
+                objectToTest.create(teamMemberPostRequestDto);
+            }).isInstanceOf(NotFoundException.class)
+                    .hasMessage(MessageFormat.format(ErrorConstant.NO_TEAM_FOUND_FOR_ID, teamMemberPostRequestDto.getTeamId()));
+        }
+
+        @Test
+        void create_shouldMapDtoInTeamMember() {
+            objectToTest.create(teamMemberPostRequestDto);
+
+            Mockito.verify(modelMapperService).map(Mockito.any(TeamMemberDto.class), Mockito.any(TeamMember.class));
+        }
+
+        @Test
+        void create_shouldSaveNewEntity() {
+            objectToTest.create(teamMemberPostRequestDto);
+
+            Mockito.verify(teamMemberRepository).save(Mockito.any(TeamMember.class));
+        }
+
+        @Test
+        void create_shouldReturnSavedTeamMember() {
+            TeamMember result = objectToTest.create(teamMemberPostRequestDto);
+
+            Assertions.assertThat(result).isEqualTo(savedTeamMember);
+        }
+
+        @Test
+        void create_returnedTeamMemberShouldContainLinkedEntities() {
+            Mockito.when(teamMemberRepository.save(Mockito.any(TeamMember.class))).then(AdditionalAnswers.returnsFirstArg());
+
+            TeamMember result = objectToTest.create(teamMemberPostRequestDto);
+
+            Assertions.assertThat(result.getTeam()).isEqualTo(teamToLink);
+            Assertions.assertThat(result.getUserInfo()).isEqualTo(userInfoToLink);
+        }
+    }
+
+    @Nested
+    class updateTest {
+        private Long givenId = 1L;
+        private TeamMember teamMemberToUpdate = TeamMemberMother.sample().build();
+        private TeamMemberDto.PostRequest teamMemberUpdateDto = TeamMemberDtoMother.postRequestSample().build();
+        private TeamMember savedTeamMember = TeamMemberMother.sample().build();
+
+        @BeforeEach
+        void setup() {
+            Mockito.when(teamMemberRepository.findById(givenId)).thenReturn(Optional.ofNullable(teamMemberToUpdate));
+            Mockito.when(teamMemberRepository.save(Mockito.any(TeamMember.class))).thenReturn(savedTeamMember);
+        }
+
+        @Test
+        void update_noTeamMemberForGivenId_throwNotFoundException() {
+            Mockito.when(teamMemberRepository.findById(givenId)).thenReturn(Optional.empty());
+
+            Assertions.assertThatThrownBy(() -> {
+                objectToTest.update(givenId, teamMemberUpdateDto);
+            }).isInstanceOf(NotFoundException.class)
+                    .hasMessage(MessageFormat.format(ErrorConstant.NO_TEAM_MEMBER_FOUND_FOR_ID, givenId));
+        }
+
+        @Test
+        void update_shouldMapDtoInTeamMember() {
+            objectToTest.update(givenId, teamMemberUpdateDto);
+
+            Mockito.verify(modelMapperService).map(Mockito.any(TeamMemberDto.class), Mockito.any(TeamMember.class));
+        }
+
+        @Test
+        void update_shouldSaveNewEntity() {
+            objectToTest.update(givenId, teamMemberUpdateDto);
+
+            Mockito.verify(teamMemberRepository).save(Mockito.any(TeamMember.class));
+        }
+
+        @Test
+        void update_shouldReturnSavedTeamMember() {
+            TeamMember result = objectToTest.update(givenId, teamMemberUpdateDto);
+
+            Assertions.assertThat(result).isEqualTo(savedTeamMember);
+        }
+    }
 }
