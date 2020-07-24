a550f9da0561d153c3e101f58f1fd79638eabfb5
Valaragen <rudy.boinnard@gmail.com>
2020-07-25 00:04:34 +0200

    register user the first time he log in the API

---
+++ usargis-api/src/main/java/com/usargis/usargisapi/web/controller/interceptor/InterceptorAppConfig.java
@@ -0,0 +1,17 @@
+package com.usargis.usargisapi.web.controller.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class InterceptorAppConfig implements WebMvcConfigurer {
+    @Autowired
+    ApiInterceptor apiInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiInterceptor);
+    }
+}
